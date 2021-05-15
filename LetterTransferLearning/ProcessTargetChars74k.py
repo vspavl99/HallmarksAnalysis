bfd9b5e7953eb77b1d432b5ed386670d7a456887
vpavlishen <v.pavlishen@businessdecision.ru>
2021-05-15 20:47:47 +0300

    Chars74k dataset added

--- LetterTransferLearning/ProcessTargetChars74k.py
+++ LetterTransferLearning/ProcessTargetChars74k.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('../Data/ClassificationYear/data.csv')
 
@@ -10,7 +11,14 @@ data['target'] = data['target'] - 10
 # combine capital and lowercase letters
 data['target'] = data['target'].map(lambda x: x - 26 if x >= 26 else x)
 
+
+# split into train and test
+data_frame_train, data_frame_val = train_test_split(
+    data, test_size=0.25, random_state=1,
+    stratify=data['target']
+)
+
 # Save the result
+data_frame_train.to_csv('newData_train.csv')
+data_frame_train.to_csv('newData_val.csv')
 data.to_csv('newData.csv')
-
-
